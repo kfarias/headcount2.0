da6dd2d00cd4a8039bc4f2407f1fdb86ebdbee16
Kenia Farias <11431kenia@gmail.com>
2017-03-15 20:22:38 -0600

    add search component that holds input field

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,4 +1,20 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card';
 
-import './Search.css'
+import './Search.css';
+
+class Search extends Component {
+  constructor() {
+    super()
+  }
+render(){
+  return(
+    <navbar>
+      <input placeholder='Enter a school district'
+             type='text'></input>
+    </navbar>
+    )
+  }
+}
+
+export default Search;
