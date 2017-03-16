59f00e3d419625e480469cf2cb5733a3ed84ec89
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-16 13:56:05 -0600

    Add classNames to cardwrapper for CSS.

--- src/CardWrapper/CardWrapper.js
+++ src/CardWrapper/CardWrapper.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import Card from '../Card/Card';
 import DistrictRepository from '../DistrictRepository';
 
-import './CardWrapper.css';
+import styles from './CardWrapper.css';
 
 const CardWrapper = ({ kindergartnerData }) => {
 
   const kinderKeysData = Object.keys(kindergartnerData).map((value, i) => {
 
     return(
-      <div key={i}>
-        <p>{value}</p>
+      <div key={i} className="individual-card">
+        <div className="location-data">{value}</div>
         <Card location={ value } data={kindergartnerData[value].data}/>
       </div>
     )
