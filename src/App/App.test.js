fd2f3a1462b4efc87680ade24db671c8990498a5
Kenia Farias <11431kenia@gmail.com>
2017-03-17 10:02:18 -0600

    conflict

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -5,14 +5,13 @@ import App from './App';
 import { shallow, mount } from 'enzyme';
 
 describe('App component', () => {
-  describe('testing App component', () => {
     it('renders without crashing', () => {
       const div = document.createElement('div');
     });
-
     it.skip('has data in an object', () => {
       const wrapper = shallow(<App/>);
       console.log(wrapper)
       expect(wrapper.props().data).to.be.defined;
       expect(wrapper.props().search).to.be.defined;
     });
+  });
