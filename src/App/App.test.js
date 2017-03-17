2661eb4a05ff8abe376cd4afb348530a84d1c502
Kenia Farias <11431kenia@gmail.com>
2017-03-17 09:41:16 -0600

    add test but is skipped

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-it.skip('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
+describe('App', ()=> {
+  it.skip('renders without crashing', () => {
+    const div = document.createElement('div');
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('div')).to.have.length(2);
+
+  });
+
 });
