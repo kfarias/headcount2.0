d7acb6409b99ddafb2cd83b2a85ef56b876a86fa
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-17 09:44:02 -0600

    Skip one test for app.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it.skip('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
+import App from './App';
+// import { shallow } from 'enzyme';
+
+describe('App component', () => {
+  describe('testing App component', () => {
+
+    it.skip('has data in an object', () => {
+      const wrapper = shallow(<App/>);
+      console.log(wrapper)
+      expect(wrapper.props().data).to.be.defined;
+      expect(wrapper.props().search).to.be.defined;
+    });
+
+  });
 });
