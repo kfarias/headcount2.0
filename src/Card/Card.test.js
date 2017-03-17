235ab50af9d9665f09303a3d414987cd732c4930
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-17 09:44:38 -0600

    skip one test for card.

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -1,7 +1,15 @@
 import DistrictRepository from '../../src/DistrictRepository.js';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
 
-it.skip('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-});
+// import { shallow, mount } from 'enzyme';
+
+describe('Card', () => {
+
+  it.skip('has  one div', () => {
+    const wrapper = shallow(
+      <Card />
+    );
+
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+})
